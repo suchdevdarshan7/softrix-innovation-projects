a4cec2af9d44680b5219e1e0cd27fcd4f6900fdc
suchdevdarshan7 <suchdev7@gmail.com>
2026-03-04 22:46:34 +0530

    Push

---
+++ Day 19/index.js
@@ -0,0 +1,22 @@
+// a = 27;
+// console.log("The value is "+a );
+// a = "Some Name";
+//
+// console.log("The modified value is "+a)
+//
+// Number
+
+a = 27;
+
+// String
+
+b = "Hello!"
+
+
+// Boolean
+
+c = true;
+
+console.log(a)
+console.log(b)
+console.log(c)
