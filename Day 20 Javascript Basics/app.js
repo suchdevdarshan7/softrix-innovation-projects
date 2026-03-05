5de4ac4231a5b261d7564c1f55609fcd97a8e3bc
suchdevdarshan7 <suchdev7@gmail.com>
2026-03-05 19:49:01 +0530

    Added files

---
+++ Day 20 Javascript Basics/app.js
@@ -0,0 +1,46 @@
+// var some_name;  // declaration
+// console.log(some_name);
+// some_name = 10; // Intiaziation
+// console.log(some_name);
+// var variable_name = "master";
+// console.log(variable_name);
+// var some_name; // redeclaration
+// console.log(some_name);
+// some_name = "This is random text " ;
+// console.log(some_name)
+// var variable_name = true;
+// console.log(variable_name);
+// console.log(student_name);
+// var student_name = "Ram";
+
+// Let
+// let student;
+// console.log(student);
+// student = "Some student";
+// console.log(student);
+// let name = "Rahul";
+// console.log(name);
+// let student; // Uncaught SyntaxError: Identifier 'student' has already been declared
+// student = "Good student";
+// console.log(student);
+// let student = "Idon'tknow";
+// console.log(student) Uncaught SyntaxError: Identifier 'student' has already been declared
+
+
+
+
+// console.log(hello_world);
+// let hello_world;
+
+
+// const apple ;
+// Uncaught SyntaxError: Missing initializer in const declaration
+// apple = 10;
+// const apple = 'isFruit';
+// console.log(apple)
+// const apple;
+// Uncaught SyntaxError: Identifier 'apple' has already been declared
+// apple = "isOrange";
+// app.js:42 Uncaught TypeError: Assignment to constant variable.
+// Uncaught SyntaxError: Identifier 'apple' has already been declared
+// const apple = "GoodApple"
---
+++ Day 20 Javascript Basics/notes.md
@@ -0,0 +1,4 @@
+Before declaration and intiazlation if we try to access a variable created using var keyword it is called as hoising
+
+
+Before declaration and intiazlation if we try to access a variable created using let and const keyword it is called as temporal dead zone 
